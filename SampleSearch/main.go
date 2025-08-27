9be3f2c6386ccb881f00dc2ea2a98ca3eed9ebca
M.Shamsi <shamsmahdi1384@gmail.com>
2025-08-27 11:49:41 +0330

    main.go

--- SampleSearch/main.go
+++ SampleSearch/main.go
@@ -15,5 +15,6 @@ func sampleSearch(myList []int, myNumber int) int {
 
 func main() {
 	myList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-	fmt.Println(sampleSearch(myList, 10))
+	fmt.Println(sampleSearch(myList, 10)) // 9
+	fmt.Println(sampleSearch(myList, 25)) // -1
 }
